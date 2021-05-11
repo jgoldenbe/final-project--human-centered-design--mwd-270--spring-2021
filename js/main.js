f4cc1ab674cd3a3bd07ada1402a6c85c7cb0d020
jgoldenbe <jgoldenbe@gmail.com>
2021-05-11 17:12:42 -0400

    more updates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 var htmlTitle  = document.title;
-var htmlScrollHeight = document.scrollHeight;
 var speed = 250;
 var endChar = "... ";
 var pos = 0;
@@ -16,13 +15,22 @@ function moveTitle()
     window.setTimeout("moveTitle()",speed);
 }
 
+//Scroll feature to change background color of sticky header.
 $(document).on('scroll', function(){
-  if ( $(window).scrollTop() > 500) {
-      $('#container-header').addClass('change-color');
-  } else {
-      $('#container-header').removeClass('change-color');
-  }
+    if ( $(window).scrollTop() > 80) {
+        $('#container-header').addClass('header-background-color');
+    } else {
+        $('#container-header').removeClass('header-background-color');
+    }
 });
 
+//Scroll feature to change html title on specifc height
+$(document).on('scroll', function(){
+  if ( $(window).scrollTop() > 720) {
+      $('#container-main').addClass('change-color');
+  } else {
+      $('#container-main').removeClass('change-color');
+  }
+});
 
 moveTitle();
